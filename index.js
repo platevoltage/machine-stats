bce1cc7070cb6a438b1567cdbb091036751db1f6
antieatingactivist <antieatingactivist@gmail.com>
2022-11-10 20:50:10 -0800

    moved power gadget logic to its own file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const exec = util.promisify(require('child_process').exec);
 const isDev = require('electron-is-dev');
 const os = require('node:os');
 const path = require('path');
+const getSample = require('./powerGadget');
 let win;
 const createWindow = ({x}) => {
   win = new BrowserWindow({
@@ -35,49 +36,7 @@ let tray;
 
 
 
-const parsedObject = {PerCore: new Array(20)};
 
-async function getSample() {
-  const { stdout, stderr } = await exec(`/Applications/"Intel Power Gadget"/PowerLog -resolution 1000 -duration 8 -verbose -file /dev/null`);
-  const text = stdout.split("--------------------------");
-  // console.log(stdout)
-  for (let block of text) {
-
-  
-    const lines = [];
-    for (const line of block.split("\n")) {
-        if (line.startsWith("\t")) lines.push(line.slice(1));
-        else if (!line.startsWith("-") && !line.startsWith("Done") && line !== "") lines.push(line);
-    }
-    let count = 0;
-    for (let line of lines) {
-      
-      if(line.startsWith("core")) {
-        const array = line.split(/MHz |Celcius /g).map(x => x.replace(/(core \d+ )/, ""))
-        if (typeof parsedObject.PerCore[count] !== "object") parsedObject.PerCore[count] = {};
-        const object = parsedObject.PerCore[count];
-        count++;
-        for (const item of array) {
-          const keyValuePair = item.split(":");
-          const key = keyValuePair[0]
-          if(!key.startsWith("core")) object[key] = keyValuePair[1]?.trim().split(' ')[0];
-        }
- 
-      } else {
-        const keyValuePair = line.split(/:|\?/g);
-        if (keyValuePair[1]) parsedObject[keyValuePair[0]] = keyValuePair[1]?.trim().split(' ')[0];
-      }
-    }
-  }
-
-  
-  if (parsedObject["package temperature"]) tray.setTitle(
-
-    `${parsedObject["package temperature"].split(".")[0]}°`
-
-  );
-
-}
 
 app.whenReady().then(() => {
   const icon = nativeImage.createFromPath('icon.png');
@@ -85,9 +44,14 @@ app.whenReady().then(() => {
   tray = new Tray(icon);
   ipcMain.on('getWindowHeight', (e, height) => win.setSize(250, height+30));
 
-  setInterval(() => {
-    getSample();
-    win?.webContents.send('sendData', parsedObject);
+  setInterval(async () => {
+    const data = await getSample();
+    if (data["package temperature"]) tray.setTitle(
+  
+      `${data["package temperature"].split(".")[0]}°`
+  
+    );
+    win?.webContents.send('sendData', data);
   }, 1000)
 
   const contextMenu = Menu.buildFromTemplate([
