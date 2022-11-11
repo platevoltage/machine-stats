e0baa3eeaa44489bacbc0cd9d49d1383a462163e
antieatingactivist <antieatingactivist@gmail.com>
2022-11-11 14:51:30 -0800

    moved tray to its own component

--- index.js
+++ index.js
@@ -2,41 +2,10 @@ const { app, screen, Tray, Menu, nativeImage, BrowserWindow, contextBridge, ipcR
 const isDev = require('electron-is-dev');
 const path = require('path');
 const getSample = require('./helpers/powerGadget');
-
 const createGraphTray = require('./components/tray');
 
 
 let data = {};
-// const createGraphCanvas = () => {
-//   const graphCanvas = new BrowserWindow({
-//     show: false,
-//     useContentSize: true,
-//     titleBarStyle: 'hidden',
-//     webPreferences: {
-//       nodeIntegration: false,
-//       contextIsolation: true,
-//       preload: path.join(__dirname, 'preload.js')
-//     }
-//   })
-//   graphCanvas.loadFile('helpers/utilizationGraphCanvas/index.html');
-//   const tray = new Tray(nativeImage.createEmpty());
-//   ipcMain.on('getGraph', (event, graphData) => {
-//     const image = nativeImage.createFromDataURL(graphData);
-//     tray.setImage( image );
-//   });
-//     const contextMenu = Menu.buildFromTemplate([
-//     { label: "open", click: () => createMainPopup(data, screen.getCursorScreenPoint())}
-//   ]);
-//   tray.setToolTip('This is my application.')
-//   tray.setContextMenu(contextMenu);
-//   setInterval(() => {
-//     if ("package temperature" in data) {
-//       tray.setTitle(`${data["package temperature"].split(".")[0]}°`);
-//       graphCanvas?.webContents.send('sendData', { data, color: accentColor});
-//     }
-//   }, 1000)
-// };
-
 
 
 app.whenReady().then(async () => {
